2025f92e42f87eaa3a31ce173a191f96d87ebf1d
amundmr <amund.raniseth@gmail.com>
2022-01-19 18:06:10 +0100

    Made function for reading gcpl data by import ecdh as module, and made UML

--- ecdh/__init__.py
+++ ecdh/__init__.py
@@ -3,3 +3,10 @@
 
 #print("Damn, you just imported ecdh as a module! That is not a supported function yet.")
 
+def get_GC(filepath, am_mass = None, specific_cycles = None):
+    import ecdh.cell as cell
+    cellobj = cell.Cell(filepath, am_mass, specific_cycles)
+    cellobj.get_data()
+    cellobj.edit_GC()
+    
+    return cellobj.GCdata
