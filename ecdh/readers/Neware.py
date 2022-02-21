aad951da5efd89beb4488b80eaa651c76bb27f42
MatsRodne <mats-a-r@hotmail.com>
2022-02-21 19:46:42 +0100

    Added: Function to delete rows. Function to adjust cycle number error. Function to iterate over time between each cycle.

--- ecdh/readers/Neware.py
+++ ecdh/readers/Neware.py
@@ -2,11 +2,12 @@
 """Data-readers for Neware"""
 # Note: If the csv-files are edited in Excel they get " added to the start of the first column. This format change confuses program.
 import pandas as pd
+import os
 
-def csv_to_df(filepath):
-    
+def read_csv(filepath):
+
     # Specified in order to see the all relevant columns of the Dataframe
-    pd.set_option('display.max_rows', 10)
+    pd.set_option('display.max_rows', 40)
     pd.set_option('display.max_columns', 10)
     pd.set_option('display.width', 1000)
     
@@ -24,12 +25,13 @@ def csv_to_df(filepath):
     # This reads the csv file with some extra options
     data = pd.read_csv( filepath,
                        usecols = col_list,          # Defines which columns you want to collect data from
+                       encoding = 'ANSI',
                        #header=0,                   # Needs to be 0 in order to find the headlines
                        #skiprows = 0,               # Needs to be 0 in order to find the headlines
                        sep=",\t",                   # Removes the odd \t delimiters. Critical.
                        engine='python',             # Don't remeber why I needed this one. Something with calc time?
                        skipinitialspace=True,       # Don't remeber why I needed this one. Something with calc time?
-                       #nrows = 60                   # This will chose how many lines you take out from the csv file. Convenient when handling massive files..
+                       #nrows = 70                   # This will chose how many lines you take out from the csv file. Convenient when handling massive files..
                        )                  
     
     # Defining temp list to store untreated data from csv file. 
@@ -38,20 +40,22 @@ def csv_to_df(filepath):
     scd = data[col_list[3]]          # Colunm marked as "Specific Capacity Discharge" in csv file
     num = data[col_list[0]]          # Colunm marked as "Cycle ID" in csv file
     
+    
     def time_secs(time_s):
         #Takes neware string of time and returns float in seconds
         time_s = time_s
         numb = time_s.split(":")
         try:
-            day = float(numb[-4])
+            #day = float(numb[-4])
             hours = float(numb[-3])
             mins = float(numb[-2])
             secs = float(numb[-1])
-            time = hours*3600 + mins*60 + secs + day*86400
+            time = hours*3600 + mins*60 + secs #+day*86400
         except IndexError:
             secs = float(numb[-1])
             time = secs
         return time
+    
     
     def cdr(a4):
         # Function to separate charge/discharge/rest
@@ -68,6 +72,7 @@ def csv_to_df(filepath):
             a6 = 'False'
             
         return a1,a6
+    
     
     def sorting():
         # This function takes out the values from the csv file, handles them, and puts them in the correct lists. 
@@ -92,8 +97,8 @@ def csv_to_df(filepath):
             # If we the csv column element is ',' it indicates 'Normal' line.
             except ValueError:
                 if num[k] == ',':                       # Indicates "Normal"
-                    b2 = time_secs(cdc[k])
-                    a2 = float(b2)    
+                    c2 = time_secs(cdc[k])
+                    a2 = float(c2)
                     a3 = float(scc[k])         
                     a4 = float(scd[k])
                     a1 = cdr(a4)[0]
@@ -106,21 +111,78 @@ def csv_to_df(filepath):
             I.append(a4)        
             cyc.append(a5) 
             chrg.append(a6) 
-    
+                    
         sl = [mode,time,V,I,cyc,chrg]
+        return sl
+
+    
+    def dup(sl):
+        # Removes duplicates
+        ind = []
+        for i in range(2,len(time)):
+            #a0 = 8
+            if time[i] == time[i-1]:
+                ind.append(i)
+        
+        for index in sorted(ind, reverse=True):
+            del mode[index]
+            del time[index]
+            del V[index]
+            del I[index]
+            del cyc[index]
+            del chrg[index]
+        return sl
+    
+    def fc(sl):
+        # Gives first cycle a cycle number
+        ind = []
+        ind2 = []    
+        nd = cyc.index(2)
+        for i in range(5,nd):
+            if mode[i] == 3:
+                ind.append(i)
+            elif mode[i] == 1:
+                ind2.append(i)
+           
+        for index in sorted(ind, reverse=True):
+            cyc[index] = 0
+            
+        for index in sorted(ind2, reverse=True):
+            cyc[index] = 1
+        
+        return sl
+        
+    def time_it(sl):
+        # Function add the time of the cycles as they iterate. 
+        # Standard of Neware is to count the accurate time per cycle, and resets it between each cycle. 
+        ti = [i for i, n in enumerate(time) if n == 0][1]
+        for i in range(ti,len(time)):
+            time[i] = time[i]+time[i-1]
         return sl
     
     def frame(sl):
         # Making a dataframe from lists. Not general, only made for this purpose. 
         # The name of the elements in the superlist
-        sl_n = ["mode", "time/s", "Ewe/V", "<I>/A", "cycle number", "charge"]           
+        sl_n = ["mode", "time/s", "Ewe/V", "<I>/mA", "cycle number", "charge"]           
         df_sl = pd.DataFrame(list(zip(sl[0],sl[1],sl[2],sl[3],sl[4],sl[5])), columns = sl_n)
         return df_sl
     
     # Calling the sorting function lists
     sfl = sorting()         # Sorted lists
     
-    # Calling the dataframe function to make a dataframe out of the sorting function lists
-    df = frame(sfl)        # Sorted dataframe
+    # This function removes duplicated rows
+    sfl2 = dup(sfl)
     
+    # This function adjusts error with numbering the first cycle
+    sfl3 = fc(sfl2)
+    
+    # This function adds the prevoius cycle time to the next cycle. Neware only measures time per cycle, and resets time between each cycle.
+    sfl4 = time_it(sfl3)
+    
+    # Calling the dataframe function to make a dataframe out of the sorting function lists
+    df = frame(sfl4)        # Sorted dataframe
+    
+    df.experiment_mode = 1
+    df.name = os.path.basename(filepath)
+
     return df
