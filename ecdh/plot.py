81857cdbfc5e8610870ac609aa5a63d6688140f2
amundmr <amund.raniseth@gmail.com>
2022-03-25 12:49:32 +0100

    if >10 files, tableu20 palette is automatically used

--- ecdh/plot.py
+++ ecdh/plot.py
@@ -56,8 +56,22 @@ class Plot:
 
         
         # List of available colors
-        self.colors =  ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan',          'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan','tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan' ]
-
+        
+        
+        if numfiles > 10:
+            tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),  
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),  
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),  
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),  
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+            for i in range(len(tableau20)):  
+                r, g, b = tableau20[i]  
+                tableau20[i] = (r / 255., g / 255., b / 255.)
+            self.colors = tableau20
+            LOG.warning("You have chosen to plot more than 10 files, which will look messy, but you do you.")
+        else:
+            tableau10 = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+            self.colors = tableau10
         # Initiate figure and axes
         if self.subplots > 2: # Then two columns, or more.
             rows = int(sqrt(self.subplots))
@@ -165,7 +179,13 @@ class Plot:
         
 
     def cycle_color(self, ncycle):
-        colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+        tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),  
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),  
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),  
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),  
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+        tableau10 = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+        colors = tableau20
         return colors[self.specific_cycles.index(ncycle)]
 
     def colormap(self, color):
