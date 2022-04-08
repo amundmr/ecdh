a817edf9f0421f5ebfe1b7b742fcd7b6ac511187
amundmr <amund.raniseth@gmail.com>
2022-04-08 18:59:11 +0200

    wow more cfg work

--- ecdh/make_toml.py
+++ ecdh/make_toml.py
@@ -56,14 +56,28 @@ print_capacities = false        # Will print the capacity of the plotted cycles 
         LOG.info("Wrote example configuration to 'ecdh.toml' with %.0f files found"%len(files))
 
 cfg_dict = {"settings": {
-                "qcplot": False,
-                "coulombicefficiency": True,
-                "percentage": False,
+                "qcplot":               (False,"Wether or not to plot capacity vs cycles") ,
+                "coulombicefficiency":  (True, "True will plot CE on twin x axis with cycle life in qcplot"),
+                "percentage":           (False, "Wether or not to use percentage in capacity vs cycles plot"),
+                "rawplot":              (True, "Wether or not to plot Potential and Current vs time"),
+                "rawplot_capacity":     (False, "Wether or not to use cumulative capacity on x-axis"),
+                "vcplot":               (False, "Wether or not to plot voltage curves (either CV og GC depending on data)"),
+                "hysteresisview":       (False, "True will remove the absolute and reset of capacity for better hysteresis viewing"),
+                "dqdvplot":             (False, "Wether or not to plot dQ/dV plots from the GCPL data"),
+                "specific_cycles":      (False, "Will make global limit of cycles, can be range or list of cycles"),
+                "cycle_range":          (False, "Cycle range you want to plot in list format like [10, 40]"),
+                "suptitle":             (False, "Title of plot"),
+                "ylabel":               (False, "Y Abcissa label"),
+                "xlabel":               (False, "X Abcissa label"),
+                "legend_location":      ('best', "Options: 'best', 'upper right', 'upper left', 'lower left', 'lower right', 'right', 'center left', 'center right', 'lower center', 'upper center', 'center'"),
+                "all_in_one":           (False, "[NOT WORKING]Puts all different datafiles in same plot"),
+                "savefig":              (False, "Save figure, false, true or path to save to."),
                 },
             "datatreatment": {
-                "reduce_data": False,
-                "dt": 10,
-                "dv": 0.01,
-                "smooth_data": False,
+                "reduce_data":          (False, "Reduces large files by changing potential resolution to 10mV and time resolution to 10s (new file is saved at same location as input file)"),
+                "dt":                   (10, "Maximum time which goes by unrecorded"),
+                "dv":                   (0.01, "Maximum voltage change which goes by unrecorded"),
+                "smooth_data":          (False, "removes outliers, new file saved at same location as input file"),
+                "print_capacities":      (False, "Will print the capacity of the plotted cycles within a potential range, can be false or list of tuples, eg: [(3.8, 4.5), (4.5, 5.0)]"),
                 }
             }
